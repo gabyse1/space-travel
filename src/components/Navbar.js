7f02098a75fc7f35c6af15873fb15aa3c8cbe5d6
Gaby <gaby.es.iaa@gmail.com>
2022-03-08 15:48:45 -0500

    Create Navbar component' structure

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import { NavLink } from 'react-router-dom';
+import logo from '../logo.png';
+
+const Navbar = () => {
+  const links = [
+    { id: 1, path: '/', text: 'ROCKETS' },
+    { id: 2, path: '/missions', text: 'MISSIONS' },
+    { id: 3, path: '/dragons', text: 'DRAGONS' },
+    { id: 4, path: '/profile', text: 'PROFILE' },
+  ];
+
+  return (
+    <nav className="navbar__box">
+      <NavLink className="navbar__brand nav-link" to="/"><img src={logo} alt="Logo" /></NavLink>
+      <button type="button" className="navbar__menu-button">
+        <span className="navbar__menu-bar" />
+      </button>
+      <ul className="navbar__menu-list">
+        {
+          links.map((link) => <li key={link.id} className="navbar-menu-item"><NavLink to={link.path}>{link.text}</NavLink></li>)
+        }
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
