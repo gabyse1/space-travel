b19afd84f080dbb6809d45fbbaf8c90a787d9d47
gabyse1 <gaby.es.iaa@gmail.com>
2022-03-09 12:53:06 -0500

    Update className attributes in Dragon component

--- src/components/Dragon.js
+++ src/components/Dragon.js
@@ -1,13 +1,13 @@
 import { PropTypes } from 'prop-types';
 
 const Dragon = ({ dragon }) => (
-  <li className="dragon__list-item">
-    <div className="list_item-image">
+  <li className="dragon__list-item rocket__list-item">
+    <div className="list__item-image">
       <img src={dragon.flickr_images[0]} alt={dragon.flickr_images[0]} />
     </div>
     <div className="list__item-details">
       <h2>{dragon.name}</h2>
-      <h2>{dragon.type}</h2>
+      <h3>{dragon.type}</h3>
       <button type="button" className="btn-blue">Reserve Dragon</button>
     </div>
   </li>
