02e0e2b64df793ab157b6af4cf2a99598a4c0cec
gabyse1 <gaby.es.iaa@gmail.com>
2022-03-09 05:43:19 -0500

    Dispatch an action to store the rockets data in Redux store

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,30 +1,17 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getRocketsAPI } from '../redux/rockets/rocketsReducer';
 import Rocket from './Rocket';
 
 const Rockets = () => {
-  const rocketsList = [
-    {
-      id: 1,
-      description: 'The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth.',
-      rocket_name: 'Falcon 1',
-      flickr_images: [
-        'https://imgur.com/DaCfMsj.jpg',
-        'https://imgur.com/azYafd8.jpg',
-      ],
-    },
-    {
-      id: 2,
-      description: 'Falcon 9 is a two-stage rocket designed and manufactured by SpaceX for the reliable and safe transport of satellites and the Dragon spacecraft into orbit.',
-      rocket_name: 'Falcon 9',
-      flickr_images: [
-        'https://farm1.staticflickr.com/929/28787338307_3453a11a77_b.jpg',
-        'https://farm4.staticflickr.com/3955/32915197674_eee74d81bb_b.jpg',
-        'https://farm1.staticflickr.com/293/32312415025_6841e30bf1_b.jpg',
-        'https://farm1.staticflickr.com/623/23660653516_5b6cb301d1_b.jpg',
-        'https://farm6.staticflickr.com/5518/31579784413_d853331601_b.jpg',
-        'https://farm1.staticflickr.com/745/32394687645_a9c54a34ef_b.jpg',
-      ],
-    },
-  ];
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRocketsAPI());
+  }, []);
+
+  const rocketsList = [];
+
   return (
     <section className="page__section">
       <ul className="rocket__list">
