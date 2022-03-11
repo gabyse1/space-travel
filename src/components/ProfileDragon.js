eaa9e5777fd23fbe245885e7674c30437bbdd70e
gabyse1 <gaby.es.iaa@gmail.com>
2022-03-10 20:06:13 -0500

    List reserved dragons

--- src/components/ProfileDragon.js
+++ src/components/ProfileDragon.js
@@ -1,36 +1,21 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 const ProfileDragon = () => {
-  const myDragons = [
-    {
-      id: 'dragon1',
-      name: 'Dragon 1',
-      type: 'capsule',
-      flickr_images: [
-        'https://i.imgur.com/9fWdwNv.jpg',
-        'https://live.staticflickr.com/8578/16655995541_7817565ea9_k.jpg',
-        'https://farm3.staticflickr.com/2815/32761844973_4b55b27d3c_b.jpg',
-        'https://farm9.staticflickr.com/8618/16649075267_d18cbb4342_b.jpg',
-      ],
-      reserved: true,
-    },
-    {
-      id: 'dragon2',
-      name: 'Dragon 2',
-      type: 'capsule',
-      flickr_images: [
-        'https://farm8.staticflickr.com/7647/16581815487_6d56cb32e1_b.jpg',
-        'https://farm1.staticflickr.com/780/21119686299_c88f63e350_b.jpg',
-        'https://farm9.staticflickr.com/8588/16661791299_a236e2f5dc_b.jpg',
-      ],
-      reserved: true,
-    },
-  ];
+  const [arrReserved, setArrReserved] = useState([]);
+  const myDragons = useSelector((state) => state.dragonsReducer);
+  useEffect(() => {
+    setArrReserved(myDragons.filter((obj) => obj.reserved));
+  }, []);
 
   return (
     <div className="profile__group">
       <h2>My Dragons</h2>
       <ul className="profile__list">
         {
-          myDragons.length > 0 ? myDragons.filter((obj) => obj.reserved).map((obj) => <li key={obj.id} className="profile__list-item">{obj.name}</li>) : <p>You haven&apos;t booked any dragons yet.</p>
+          arrReserved.length > 0
+            ? arrReserved.map((obj) => <li key={obj.id} className="profile__list-item">{obj.name}</li>)
+            : <p>You haven&apos;t booked any dragons yet.</p>
         }
       </ul>
     </div>
