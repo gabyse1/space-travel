cfefcf73f346b4e870f1d480519ce46d1aab2918
gabyse1 <gaby.es.iaa@gmail.com>
2022-03-10 20:05:59 -0500

    List joined missions

--- src/components/ProfileMission.js
+++ src/components/ProfileMission.js
@@ -1,25 +1,21 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 const ProfileMission = () => {
-  const myMissions = [
-    {
-      mission_name: 'Thaicom',
-      mission_id: '9D1B7E0',
-      description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
-      reserved: true,
-    },
-    {
-      mission_name: 'Telstar',
-      mission_id: 'F4F83DE',
-      description: 'Telstar 19V (Telstar 19 Vantage) is a communication satellite in the Telstar series of the Canadian satellite communications company Telesat. It was built by Space Systems Loral (MAXAR) and is based on the SSL-1300 bus. As of 26 July 2018, Telstar 19V is the heaviest commercial communications satellite ever launched, weighing at 7,076 kg (15,600 lbs) and surpassing the previous record, set by TerreStar-1 (6,910 kg/15230lbs), launched by Ariane 5ECA on 1 July 2009.',
-      reserved: true,
-    },
-  ];
+  const [arrJoined, setArrJoined] = useState([]);
+  const myMissions = useSelector((state) => state.missionsReducer);
+  useEffect(() => {
+    setArrJoined(myMissions.filter((obj) => obj.reserved));
+  }, []);
 
   return (
     <div className="profile__group">
       <h2>My Missions</h2>
       <ul className="profile__list">
         {
-          myMissions.length > 0 ? myMissions.filter((obj) => obj.reserved).map((obj) => <li key={obj.mission_id} className="profile__list-item">{obj.mission_name}</li>) : <p>You haven&apos;t joined any mission yet.</p>
+          arrJoined.length > 0
+            ? arrJoined.map((obj) => <li key={obj.mission_id} className="profile__list-item">{obj.mission_name}</li>)
+            : <p>You haven&apos;t joined any mission yet.</p>
         }
       </ul>
     </div>
